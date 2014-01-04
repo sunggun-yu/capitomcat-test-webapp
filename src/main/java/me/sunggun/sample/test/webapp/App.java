0b3565da2b6b6dbcf43d2c339510227fcf035eda
Sunggun Yu <sunggun.yu@gmail.com>
2014-01-03 23:05:40 -0500

    Initial commit

---
+++ src/main/java/me/sunggun/sample/test/webapp/App.java
@@ -0,0 +1,11 @@
+package me.sunggun.sample.test.webapp;
+
+/**
+ * @author $Author$
+ * @version $Rev$
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<h3>groupId : me.sunggun.sample</h3>
+<h3>artifactId : test-webapp</h3>
+</body>
+</html>
